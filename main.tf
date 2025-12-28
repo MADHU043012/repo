17e69dc3e332e56d7b269cc26469ea407c9022ee
Madhuri <madhu04kamble@gmail.com>
2025-12-28 23:58:16 +0530

    update terraform main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 terraform {
-    backend "s3"
+    backend "s3" {
     bucket = "terraform-backup-s3-2025"
     region = "ap-southeast-2" 
        key = "tfstate"
+    }
 }
 
 provider "aws" {
@@ -11,9 +12,9 @@ provider "aws" {
     region  = "ap-southeast-2"
 }
 resource "aws_instance" "EC2-instance" {
-    ami ="ami-0b3c832b6b7289e44"
-    instance_type ="t3.micro"
-  key_name =  "server-key"
+    ami =  var.ami_id
+    instance_type =var.instance_type
+  key_name =  var.key_name
 vpc_security_group_ids  =["sg-01acedc7f409bec06"]
 availability_zone ="ap-southeast-2a"
 tags ={
@@ -21,6 +22,30 @@ tags ={
     
  }
 }
+
+  variable "ami_id" {
+    default ="ami-0b3c832b6b7289e44"
+ description = "this is my new ami_id"
+
+  }
+variable  "instance_type" {
+default      = "t3.micro"
+ description  = "EC2 instance type"
+}
+
+variable  "key_name"{
+    description  = "EC2 key pair name"
+  default        = "server-key"
+
+}
+
+
+  
+  
+
+
+
+
 
   
 
